48ff3056c80c0bedba13cfe3a2ac7b7319403911
Gyles Batara <gyles.batara@live.com>
2018-02-05 09:10:27 -0800

    Fill out second div by adding a variable that holds both the greeting and the clock

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 var greeting = React.createElement('h1', {}, 'I am a react project');
+var clock = React.createElement('h2', {}, `It is ${new Date().toLocaleTimeString()}`);
+var app = React.createElement('div', {}, greeting, clock);
 
 ReactDOM.render(
-  greeting,
+  app,
   document.getElementById('react-app-root')
 );
